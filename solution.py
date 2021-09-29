1cbeca839338b034346c083b844d822bc2a57f8d
Collin Stilwell <cns9383@nyu.edu>
2021-09-29 00:34:22 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,7 +6,6 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     email = "cns9383@nyu.edu \r\n"
 
     # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
-    mailserver
 
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
     clientSocket = socket(AF_INET, SOCK_STREAM)
