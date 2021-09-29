71812e35a39575d2b7142fa486c61030e3c4063b
Collin Stilwell <cns9383@nyu.edu>
2021-09-29 13:11:48 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,48 +5,42 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     endmsg = "\r\n.\r\n"
     email = "cns9383@nyu.edu \r\n"
 
-    # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
-
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((mailserver,port))
 
-    recv = clientSocket.recv(1024).decode()
-
     # Send HELO command and print server response.
     heloCommand = 'HELO Alice\r\n'
     clientSocket.send(heloCommand.encode())
-    recv
+    clientSocket.recv(1024).decode()
 
     # Send MAIL FROM command and print server response.
     mailFrom = "MAIL FROM: " + email
     clientSocket.send(mailFrom.encode())
-    recv
+    clientSocket.recv(1024).decode()
 
     # Send RCPT TO command and print server response.
     rcptTo = "RCPT TO: " + email
     clientSocket.send(rcptTo.encode())
-    recv
-    
+    clientSocket.recv(1024).decode()
+
     # Send DATA command and print server response.
     data = "DATA \r\n"
     clientSocket.send(data.encode())
-    recv
-    
+    clientSocket.recv(1024).decode()
+
     # Send message data.
     messageSubject = "Subject: Test Test Test \r\n\r\n"
     clientSocket.send(messageSubject.encode())
     clientSocket.send(msg.encode())
     clientSocket.send(endmsg.encode())
-    recv
-    
-    # Message ends with a single period.
-    #TODO
+    clientSocket.recv(1024).decode()
 
     # Send QUIT command and get server response.
     quit = "QUIT \r\n"
     clientSocket.send(quit.encode())
-    recv
+    clientSocket.recv(1024).decode()
+
 
 if __name__ == '__main__':
     smtp_client(1025, '127.0.0.1')
